366ea0c97c91d40887ad0cc55fc08509665e280f
denisetiya <denisetiya009@gmail.com>
2025-08-26 23:26:48 -0700

    feat(middleware): add API key middleware to app router
    - Import apiKeyMiddleware from middleware module
    - Apply apiKeyMiddleware globally to appRouter before auth routes
    - Enhance security by verifying API keys for incoming requests

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,8 +1,10 @@
 import express, { type Router } from 'express';
 import authRouter from './modules/auth/auth.router';
+import { apiKeyMiddleware } from './middleware';
 
 const appRouter: Router = express.Router();
 
+appRouter.use(apiKeyMiddleware)
 appRouter.use('/auth', authRouter);
 
 export default appRouter;
